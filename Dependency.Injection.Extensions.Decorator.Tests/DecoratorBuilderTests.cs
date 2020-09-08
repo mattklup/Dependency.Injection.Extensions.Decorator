4c37c6b21ce8de18a5b857e4854639e7854e2943
mattklup <mattklup@microsoft.com>
2020-09-08 14:26:32 -0700

    Add empty test project to sln

---
+++ Dependency.Injection.Extensions.Decorator.Tests/DecoratorBuilderTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Dependency.Injection.Extensions.Decorator.Tests
+{
+    public class DecoratorBuilderTests
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
